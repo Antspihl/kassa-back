4fd7b5537ad0d9aca7eea329021ae94d652addfa
inpihl <antspihl@gmail.com>
2023-06-21 13:27:21 +0300

    Creates and logs transactions into a google sheet

--- writer.py
+++ writer.py
@@ -5,44 +5,65 @@ import pygsheets
 gc = pygsheets.authorize(service_file="sheets_api.json")
 
 df = pd.DataFrame()
+df["timestamp"] = []
 df["customer_name"] = []
 df["drink_name"] = []
 df["quantity"] = []
 
 
-def create_transactions(customer_name, drink_name, quantity):
+def log_transactions(customer_name: str, drink_name: str, quantity: int, has_record: bool):
     """
     Log all transactions, e.g. who ordered what and how many. Can be used for debugging
     """
-    sh = gc.open("Sheets api test")
-    wks = sh[1]  # table name: transactions
+    sh = gc.open("Soveldaja kassa")
+    wks = sh[0]  # table name: "Tellimuste kokkuvõte"
+    log = sh[1]  # table name: "Tellimuste logi"
 
-    existing_data = wks.get_all_records()
-    print(existing_data)
+    wks_existing_data = wks.get_all_records()
+    log_existing_data = log.get_all_records()
 
-    new_row = {
+    order_data = {
         "customer_name": customer_name,
         "drink_name": drink_name,
         "quantity": quantity
     }
 
-    existing_data.append(new_row)
-    wks.set_dataframe(pd.DataFrame(existing_data), start="A1")
+    log_data = {
+        "timestamp": pd.Timestamp.now(),
+        "customer_name": customer_name,
+        "drink_name": drink_name,
+        "quantity": quantity
+    }
+
+    log_existing_data.append(log_data)
+    wks_existing_data.append(order_data)
+    log.set_dataframe(pd.DataFrame(log_existing_data), start="A1")
+
+    if not has_record:
+        wks.set_dataframe(pd.DataFrame(wks_existing_data), start="A1")
 
 
-def add_to_customer(customer_name, drink_name, quantity):
-    sh = gc.open("Sheets api test")
+def add_order(customer_name: str, drink_name: str, quantity: int):
+    """
+    Add a drink to a customer's order. If the customer already has an order, add to the existing order.
+    """
+    if quantity < 0 or customer_name == "" or drink_name == "":
+        return
+    has_record = False
+    sh = gc.open("Soveldaja kassa")
     wks = sh[0]  # table name: main
 
     existing_data = wks.get_all_records()
-    print(existing_data)
+    print(f"Existing data: {existing_data}")
 
     for row in existing_data:
-        if row["customer_name"] == customer_name:
-            print(row["quantity"])
+        if row["customer_name"] == customer_name and row["drink_name"] == drink_name:
+            has_record = True
             if pd.isna(row["quantity"]):
                 row["quantity"] = 0
             row["quantity"] += quantity
             break
+
+    log_transactions(customer_name, drink_name, quantity, has_record)
 
     wks.set_dataframe(pd.DataFrame(existing_data), start="A1")
