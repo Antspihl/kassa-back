0da12a137aab662cec17f8778e4efd1fa6990888
inpihl <antspihl@gmail.com>
2023-06-21 11:15:34 +0300

    init

---
+++ app.py
@@ -0,0 +1,43 @@
+"""Soveldaja kassa rakendus"""
+from dataclasses import dataclass
+
+from flask import Flask, request
+
+from writer import create_transactions, add_to_customer
+
+app = Flask(__name__)
+
+
+@dataclass
+class Order:
+    customer_name: str
+    drink_name: str
+    quantity: int
+
+
+@app.route("/order", methods=["POST"])
+def order():
+    """
+    Read in an order and add to the Google sheet.
+    """
+    try:
+        data = Order(request.json["customer_name"], request.json["drink_name"], request.json["quantity"])
+        add_to_customer(data.customer_name, data.drink_name, data.quantity)
+
+        return "ok"
+    except Exception as e:
+        print(e)
+        return e
+
+
+@app.route("/data", methods=["GET"])
+def data():
+    """
+    Read data from a Google sheet and return it as a json.
+    :return:
+    """
+    #  Todo: read data from a Google sheet and return it as a json
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
